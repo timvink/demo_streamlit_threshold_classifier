2345d1252cf8a8e57d4a3f3bd07a8019f271d414
Tim Vink <vinktim@gmail.com>
2020-10-20 10:20:25 +0200

    Add blogpost link in app

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ def cached_get_predictions():
 y_train, yhat_prob_train, y_test, yhat_prob_test = cached_get_predictions()
 
 # UI
-st.title("Setting the threshold for our classifier")
-st.write("Interactively using streamlit! :point_down: ")
+st.title("Setting the threshold for our classifier :point_down: ")
+st.write("Demo of interactive streamlit app, accompanying this [blogpost](https://www.timvink.nl/streamlit-threshold-app/).")
 threshold = st.slider("Threshold", min_value=0.00, max_value=1.0, step=0.01, value=0.5)
 
 # Metrics
